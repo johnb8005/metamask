719110993c416568857475fd882b545d8884b4b5
John B <johan@nexys.ch>
2022-02-15 04:56:44 +0100

    Update router.tsx

--- src/router.tsx
+++ src/router.tsx
@@ -3,7 +3,6 @@ import { Route, Switch } from "react-router-dom";
 import Home from "./home";
 import Phantom from "./phantom";
 import MetaMask from "./meta-mask";
-import Webauthn from "./webauthn";
 
 const NotFound = () => (
   <p>
@@ -17,7 +16,6 @@ export default () => {
       <Route exact path={"/"} component={Home} />
       <Route exact path={"/phantom"} component={Phantom} />
       <Route exact path={"/metamask"} component={MetaMask} />
-      <Route exact path={"/webauthn"} component={Webauthn} />
       <Route component={NotFound} />
     </Switch>
   );
